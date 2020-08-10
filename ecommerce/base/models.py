c1d472deb797c1c7ec56bdab1b5705b3072c6ba2
andremenezees <andrr96@gmail.com>
2020-08-10 05:15:49 -0300

    Adicionando funcionalidade de paginacao cart e limpando template

--- ecommerce/base/models.py
+++ ecommerce/base/models.py
@@ -1,20 +1,58 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear'),
+
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger'),
+
+)
 
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2, default='S')
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1, default='P')
+    slug = models.SlugField(default='test-product')
+    description = models.TextField(default="Text description asdoaseuwqeuqwheqwuheqkdokasokd adiasdasue huashud")
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('base:product', kwargs={
+            'slug': self.slug
+        })
+
+    def get_add_to_cart_url(self):
+        return reverse('base:add-to-cart', kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart_url(self):
+        return reverse('base:remove-from-cart', kwargs={
+            'slug': self.slug
+        })
 
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    price = models.FloatField()
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 
 class Order(models.Model):
@@ -25,4 +63,4 @@ class Order(models.Model):
     ordered = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.user.username
